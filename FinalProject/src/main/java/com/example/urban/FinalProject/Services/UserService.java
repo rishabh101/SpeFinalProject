dc83779f1079d82c99c2366bfdb86c853e14db58
rishabh101 <rishabhtalwar30@gmail.com>
2022-05-11 00:09:06 +0530

    Update UserService.java

--- FinalProject/src/main/java/com/example/urban/FinalProject/Services/UserService.java
+++ FinalProject/src/main/java/com/example/urban/FinalProject/Services/UserService.java
@@ -1,18 +1,17 @@
 package com.example.urban.FinalProject.Services;
 
-<<<<<<< HEAD
-=======
+
+
 import java.util.List;
 
->>>>>>> Backend commit
+
 import org.springframework.stereotype.Service;
 
 
 @Service
 public interface UserService {
 	boolean signUp(String fullname, String email, String address, String password );
-<<<<<<< HEAD
-=======
+
 	public long login (String emailId, String password);
->>>>>>> Backend commit
+
 }
