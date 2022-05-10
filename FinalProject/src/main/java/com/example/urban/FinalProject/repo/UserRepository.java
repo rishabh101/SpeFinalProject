04fa221ce69bd2bf4d752004af124398942883c9
rishabh101 <rishabhtalwar30@gmail.com>
2022-05-11 00:15:09 +0530

    Update UserRepository.java

--- FinalProject/src/main/java/com/example/urban/FinalProject/repo/UserRepository.java
+++ FinalProject/src/main/java/com/example/urban/FinalProject/repo/UserRepository.java
@@ -1,25 +1,24 @@
 package com.example.urban.FinalProject.repo;
 
-<<<<<<< HEAD
+
 import org.springframework.data.jpa.repository.JpaRepository;
-=======
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
->>>>>>> Backend commit
+
 import org.springframework.stereotype.Repository;
 
 import com.example.urban.FinalProject.bean.Users;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users,Long >{
-<<<<<<< HEAD
 
-=======
+
 	public boolean existsByEmail(String email);
 	public Users findByEmailAndPassword(String email, String password);
 	@Query("Select email,password from Users")
 	List<String> findEmailandPassword();
->>>>>>> Backend commit
+
 }
